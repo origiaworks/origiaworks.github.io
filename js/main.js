9d54c0aed410ba5f259cd41ad27a82ce0a2f506c
でぃー <124360236+origiaworks@users.noreply.github.com>
2025-11-03 21:53:16 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     // ヘッダーを読み込んで挿入する
-    fetch('/_header.html')
+    fetch('/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
 
     // フッターを読み込んで挿入する
-    fetch('/_footer.html')
+    fetch('/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
         });
 });
+
 
 
 
