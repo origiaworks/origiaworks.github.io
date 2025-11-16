30230273947a09200fce8a410841ab00395ebf26
でぃー <124360236+origiaworks@users.noreply.github.com>
2025-11-16 23:59:19 +0900

    Update mailsend.js

--- js/mailsend.js
+++ js/mailsend.js
@@ -11,13 +11,17 @@ function sendMail() {
 
     // メール本文を作成
     // 改行は %0A （URLエンコードされた改行コード）を使用
-    let body = "以下の情報でお問い合わせがありました。%0A%0A";
-    body += "アプリ名: " + appName + "%0A";
-    body += "ユーザーID: " + userId;
+    let body  += "ゲームデータ削除を依頼します。%0A%0A";
+    body += "-----------------------------------------%0A";
+    body += "■ アプリ名: " + appName + "%0A";
+    body += "■ ユーザーID: " + userId + "%0A";
+    body += "-----------------------------------------%0A%0A";
+    body += "※ 本メールはデータ削除依頼専用です。%0A";
+    body += "※ 本メール送信後、本人確認のため、サポートより返信させていただく場合がございます。%0A";
 
     // 宛先と件名（ここでは固定値）
     const to = "origia000@gmail.com"; // ★あなたのメールアドレスに変更してください
-    const subject = "データ削除依頼";
+    const subject = "【ORIGIAWORKS】データ削除依頼";
 
     // mailtoリンクを生成して開く
     const mailtoLink = `mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
